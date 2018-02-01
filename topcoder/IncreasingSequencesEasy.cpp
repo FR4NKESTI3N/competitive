88534a2e95722c72db15912cfba68f50a56cf732
FR4NKESTI3N <aggarwal.yash2011@gmail.com>
2018-02-01 23:01:23 +0530

    Corrected IncreasingSequencesEasy, Optimised dp table to have only 2 columns

--- topcoder/IncreasingSequencesEasy.cpp
+++ topcoder/IncreasingSequencesEasy.cpp
@@ -1,51 +1,36 @@
 #include <iostream>
-
-
 #include <vector>
-#include <algorithm>
 #include <climits>
 #include <cstring>
-
-using namespace std;
-
-const long long MOD = 998244353;
+#include <algorithm>
 
 class IncreasingSequencesEasy{
+private:
+    const long long MOD = 998244353;
 public:
-    int count(std::vector<int> l, std::vector<int> r){
-        int cnt = 0;
-        //int minl = *min_element(l.begin(), l.end());
-        int maxr = *max_element(r.begin(), r.end());
-        int memo[l.size()+1][maxr + 1];
-        memset(memo, 0, sizeof memo);
-        for(int i = l[0]; i <= r[0]; ++i)
-            memo[0][i] = 1;
-        for(int i = 0; i < l.size(); ++i){
-            // for(int j = l[i-1]; j < l[i]; ++j){
-            //     memo[i][l[i]]+=memo[i-1][j];
-            //     memo[i][l[i]] %= 998244353;
-            // }
-            // for(int j = l[i] + 1; j <= r[i]; ++j){
-            //     memo[i][j] += memo[i-1][j] + memo[i][j-1];
-            //     memo[i][j] %= 998244353;
-            // }
-            for(int j = l[i]; j <= r[i]; ++j){
-                memo[i+1][j] = memo[i][j] +  (j>0)?(memo[i][j-1]):0;
+    int count(std::vector<int> L, std::vector<int> R){
+        int maxR = *max_element(R.begin(), R.end());
+        std::vector<std::vector<long long>> dp(2,std::vector<long long> (1000000, 0));
+        for(int i = L[0]; i <= R[0]; ++i)
+            dp[0][i] = 1;
+        int i;
+        for(int I = 1; I < L.size(); ++I){
+            i = I%2;
+            fill(dp[i].begin(), dp[i].end(), 0);
+            for(int k = L[I - 1]; k < L[I]; ++k){
+                dp[i][L[I]] += dp[(i + 1)%2][k];
+                dp[i][L[I]] %= MOD;
+            }
+            for(int j = L[I] + 1; j <= R[I]; ++j){
+                dp[i][j] += dp[i][j - 1] + dp[(i + 1)%2][j - 1];
+                dp[i][j] %= MOD;
             }
         }
-        // for(int j = 0; j <= (maxr+1); ++j){
-        //     for(int i = 0; i <= l.size(); ++i)
-        //         cout<<memo[j][i]<<' ';
-        //     cout<<'\n';
-        // }
-        for(int i = 0; i <= maxr; i++)
-            cnt += memo[l.size() - 1][i];
-        return cnt;
+        int sum = 0;
+        for(int j = 0; j <= maxR; ++j){
+            sum += dp[i][j];
+            sum %= MOD;
+        }
+        return sum;
     }
 };
-
-int main(void){
-    IncreasingSequencesEasy a;
-    cout<<a.count(vector<int> {1, 3, 1, 4},vector<int> {6, 5, 4, 6});
-    return 0;
-}
